07360f6e33ea9b8257fcd1b0d50ea797e905dd00
yurixtugal <yurixtugal12@gmail.com>
2023-03-26 01:45:51 -0500

    add swagger documentation for the API

--- src/main.ts
+++ src/main.ts
@@ -1,10 +1,19 @@
 import { ValidationPipe } from '@nestjs/common';
 import { NestFactory } from '@nestjs/core';
 import { AppModule } from './app.module';
+import { SwaggerModule, DocumentBuilder } from '@nestjs/swagger';
 
 async function bootstrap() {
   const app = await NestFactory.create(AppModule);
   app.useGlobalPipes(new ValidationPipe())
-  await app.listen(3000);
+  const config = new DocumentBuilder()
+  .setTitle('School List')
+  .setDescription('The school list API')
+  .setVersion('1.0')
+  .addTag('school-list')
+  .build();
+  const document = SwaggerModule.createDocument(app, config);
+  SwaggerModule.setup('api', app, document);
+  await app.listen(process.env.PORT);
 }
 bootstrap();
